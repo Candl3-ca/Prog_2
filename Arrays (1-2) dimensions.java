5960e7f666736b090fd509e600b91abe9044adf2
Candl3-ca <75958530+Candl3-ca@users.noreply.github.com>
2022-09-02 09:31:02 -0400

    Update Arrays (1-2) dimensions.java

--- Arrays (1-2) dimensions.java
+++ Arrays (1-2) dimensions.java
@@ -1,16 +1,14 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
-
-
-public class main {
-     public static void main(String[] args) {
-
+public class Main {
+    public static void main(String[] args) {
 //         Write a program that asks for 10 numbers then searchs for a number in it.
 
 
 //         System.out.println("Enter 10 numbers");
 //         int[] list = new int[10];
-         Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 //         for (int i = 0; i < 10; i++) {
 //             list[i] = sc.nextInt();
 //         }
@@ -62,27 +60,68 @@ public class main {
 //         Taking 12 numbers and printing the in a 3 by 4 matrix as well as their sum
 
 
-//         System.out.println("Enter 12 numbers");
-//         int[] list2 = new int[12];
-//         int[][] matrix = new int[3][4];
-//
-//            for (int i = 0; i < 12; i++) {
-//                list2[i] = sc.nextInt();
-//            }
-//
-//            for (int i = 0; i < 3; i++) {
-//                for (int j = 0; j < 4; j++) {
-//                    matrix[i][j] = list2[i * 4 + j];
-//                }
-//            }
-//
-//            for (int i = 0; i < 3; i++) {
-//                for (int j = 0; j < 4; j++) {
-//                    System.out.print(matrix[i][j] + " ");
-//                }
-//                System.out.println();
-//            }
-//
-//         System.out.println("The sum of the 12 numbers is " + Arrays.stream(list2).sum());
-     }
+         System.out.println("Enter 12 numbers");
+         int[] list2 = new int[12];
+         int[][] matrix = new int[3][4];
+
+            for (int i = 0; i < 12; i++) {
+                list2[i] = sc.nextInt();
+            }
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 4; j++) {
+                    matrix[i][j] = list2[i * 4 + j];
+                }
+            }
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 4; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+         System.out.println("The sum of the 12 numbers is " + Arrays.stream(list2).sum());
+
+        System.out.println("The sum of each row is ");
+        for (int i = 0; i < 3; i++) {
+            int sum = 0;
+            for (int j = 0; j < 4; j++) {
+                sum += matrix[i][j];
+            }
+            System.out.println(sum);
+        }
+        System.out.println("The sum of each column is ");
+        for (int i = 0; i < 4; i++) {
+            int sum = 0;
+            for (int j = 0; j < 3; j++) {
+                sum += matrix[j][i];
+            }
+            System.out.println(sum);
+        }
+
+
+        System.out.println("Enter a number to search");
+        int num = sc.nextInt();
+
+        int count = 0;
+        for (int i = 0; i < 12; i++) {
+            if (list2[i] == num) {
+                count++;
+            }
+        }
+
+        System.out.println("The number " + num + " is found " + count + " times");
+
+        System.out.println("Printing rows and columns where the number is found");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (matrix[i][j] == num) {
+                    System.out.println("The number " + num + " is found at row " + (i + 1) + " and column " + (j + 1) + ".");
+                }
+            }
+        }
+
+
+    }
 }

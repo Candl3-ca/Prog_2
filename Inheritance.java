e09739862e8d01d2fd8ed6f3e1e6c64c5c2620a8
Candl3-ca <75958530+Candl3-ca@users.noreply.github.com>
2022-09-16 09:12:10 -0400

    Inheritance and super Keyword

---
+++ Inheritance.java
@@ -0,0 +1,156 @@
+import java.util.*;
+
+public class main {
+
+
+    public void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+//        Child c = new Child();
+//        System.out.println(c.b);
+//        c.b = 9;
+//        System.out.println(c.b);
+//        c.display();
+//        System.out.println(c.a);
+//        c.a = 1;
+//        System.out.println(c.a);
+//        c.show();
+//
+//        GC1 grand_child = new GC1();
+//        grand_child.show();
+//        grand_child.display();
+//        grand_child.demo();
+//
+//        Govermental op = new Govermental();
+//
+//        System.out.println("Enter the organization name ");
+//        op.setName(sc.nextLine());
+//        System.out.println("Enter the organization id ");
+//        op.setEstablishment(sc.nextLine());
+//        System.out.println("Enter the number of employees ");
+//        op.setNo_of_employee(sc.nextInt());
+//        System.out.println(op.name + op.establishment + op.No_of_employee);
+
+//        Client c = new Client();
+//        Store1 s = new Store1("s.setName");
+//        bike_store b = new bike_store(s.name, 17, true);
+//
+//
+//        System.out.println(b.toString() + c.toString());
+
+
+    }
+
+
+//class Parent{
+//    int a = 5;
+//    void show(){
+//        System.out.println("Inside the parent class");
+//    }
+//}
+//
+//class Child extends Parent{
+//    int b = 7;
+//    void display(){
+//        System.out.println("Inside the child class");
+//    }
+//}
+//
+//class P1 {
+//    void show(){
+//        System.out.println("Inside the P1 class");
+//    }
+//}
+//
+//class C1 extends P1 {
+//    void display(){
+//        System.out.println("Inside the C1 class");
+//    }
+//}
+//
+//class GC1 extends C1 {
+//    void demo(){
+//        System.out.println("Inside the GC1 class");
+//    }
+//}
+//
+//class Organization{
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getEstablishment() {
+//        return establishment;
+//    }
+//
+//    public void setEstablishment(String establishment) {
+//        this.establishment = establishment;
+//    }
+//
+//    String name;
+//    String establishment;
+//}
+//
+//class Private_sector extends Organization{
+//    String city;
+//    boolean Turn_over;
+//}
+//
+//class Govermental extends Organization{
+//    public int getNo_of_employee() {
+//        return No_of_employee;
+//    }
+//
+//    public void setNo_of_employee(int no_of_employee) {
+//        No_of_employee = no_of_employee;
+//    }
+//
+//    int No_of_employee;
+//}
+
+//    public class Store1 {
+//        public final double SALES_TAX_RATE = 0.06;
+//        private String name;
+//
+//        public Store1(String name) {
+//            this.name = name;
+//        }
+//
+//        public String getName() {
+//            return name;
+//        }
+//
+//        public void setName(String name) {
+//            this.name = name;
+//        }
+//
+//        public String toString() {
+//            return ("Name: " + name);
+//        }
+//    }
+//
+//    public class bike_store extends Store1 {
+//        int no_of_bikes;
+//        boolean sponsored_or_not;
+//
+//        public bike_store(String name, int no_of_bikes, boolean sponsored_or_not) {
+//            super(name);
+//            this.no_of_bikes = no_of_bikes;
+//            this.sponsored_or_not = sponsored_or_not;
+//        }
+//    }
+//
+//    public static class Client {
+//        String name = "AMOGUS";
+//        String client_email = "AMOGUS@gmail.com";
+//
+//        public String toString() {
+//            return ("Name: " + name + "Email: " + client_email);
+//        }
+//    }
+}

6d9b97992ff9ef752d65a0d271edd40384bd0963
Candl3-ca <75958530+Candl3-ca@users.noreply.github.com>
2022-11-24 11:09:12 -0500

    Create IO_lab.java

---
+++ IO_lab.java
@@ -0,0 +1,78 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("menu. 1- create a folder, 2- create a file, 3- write to the file, 4- show full file, 5- delete file, 6- delete folder, 7- exit");
+        int menu = sc.nextInt();
+        String fileName;
+
+
+
+        switch (menu){
+            case 1:
+                System.out.println("Enter the name of the folder");
+                String folderName = sc.next();
+                File folder = new File(folderName);
+                folder.mkdir();
+                break;
+                case 2:
+                    System.out.println("Enter the name of the file");
+                    fileName = sc.next();
+                    File file = new File(fileName);
+                    file.createNewFile();
+                    break;
+            case 3:
+                System.out.println("Enter the name of the file");
+                fileName = sc.next();
+                System.out.println("Enter the text to write to the file");
+                String text = sc.nextLine();
+                FileWriter writer = new FileWriter(fileName);
+                writer.write(text);
+                writer.close();
+                break;
+            case 4:
+                System.out.println("Enter the name of the file to read");
+                String fileName2 = sc.next();
+                File file2 = new File(fileName2);
+                Scanner sc2 = new Scanner(file2);
+                System.out.println("is readable: " + file2.canRead());
+                System.out.println("is writeable: " + file2.canWrite());
+                System.out.println("is hidden: " + file2.isHidden());
+                System.out.println("is executable: " + file2.canExecute());
+                int lineCount = 0;
+                int wordCount = 0;
+                while (sc2.hasNextLine()){
+                    String line = sc2.nextLine();
+                    lineCount++;
+                    String[] words = line.split(" ");
+                    wordCount += words.length;
+                    System.out.println(line);
+                }
+                break;
+            case 5:
+                System.out.println("Enter the name of the file to delete");
+                String fileName3 = sc.next();
+                File file3 = new File(fileName3);
+                file3.delete();
+                break;
+            case 6:
+                System.out.println("Enter the name of the folder to delete");
+                String folderName2 = sc.next();
+                File folder2 = new File(folderName2);
+                folder2.delete();
+                break;
+            case 7:
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Wrong number");
+                System.out.println("Please re-enter the number");
+                menu = sc.nextInt();
+                break;
+        }
+    }
+
+}
